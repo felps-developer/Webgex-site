1770f3a1be4a61a6fa35411e61445271ab4964cc
Felipe25687 <felipemofernandes@gmail.com>
2024-01-23 13:10:12 -0300

    ajustes no site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: FlexStart
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
